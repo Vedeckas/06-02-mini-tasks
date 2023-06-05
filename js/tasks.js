de6bcca6925bda2893c573084d526437d835bc9c
Petras <petrosenas@gmail.com>
2023-06-05 12:25:00 +0300

    First task

---
+++ js/tasks.js
@@ -0,0 +1,17 @@
+// 1. Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+const value = 1;
+const value1 = 2 ;
+const value2 = 3 ;
+const opa = (value, value1, value2);
+console.log(opa);
+
+// 2. Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+const value3 = `Namas`;
+const value4 = `Lenta`;
+const value5 = `Plyta`;
+const opa1 = `value3, value4, value5`;
+console.log(opa1);
