9dfe0605b712e03eb4d9719704fe6e478bd5f661
Petras <petrosenas@gmail.com>
2023-06-07 12:58:54 +0300

    One more

--- js/tasks.js
+++ js/tasks.js
@@ -1,11 +1,10 @@
 // 1. Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
 // Po kiekvieno jų inicijavimo, išvesti į console
 
-const value = 1;
-const value1 = 2 ;
-const value2 = 3 ;
-const opa = (value, value1, value2);
-console.log(opa);
+let value = 1;
+let value1 = 2;
+let value2 = 3;
+console.log(value, value1, value2);
 
 // 2. Sukurti 3 kintamuosius su teksto tipo reikšmėmis
 // Po kiekvieno jų inicijavimo, išvesti į console
@@ -13,5 +12,32 @@ console.log(opa);
 const value3 = `Namas`;
 const value4 = `Lenta`;
 const value5 = `Plyta`;
-const opa1 = `value3, value4, value5`;
+const opa1 = value3 + ' ' + value4 + ' ' + value5;
 console.log(opa1);
+
+// 3. Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+let value6 = (1, 2, 3, 4, 5);
+let value7 = (2, 3, 4, 5, 6);
+let value8 = (3, 4, 5, 6, 7);
+const opa2 = 'value6, value7, value8';
+console.log(opa2);
+
+// nesupratau
+function num(n1,n2,n3,n4,n5) {
+    return "\n"+n1+" "+n2+" "+n3+" "+n4+" "+n5;
+    }
+    let num4 = num(1,2,3,4,5); 
+    let num5 = num(6,7,8,9,10);
+    let num6 = num(11,12,13,14,15);
+    console.log(num4,num5,num6);
+
+// 4. Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+const value9 = (`Namas`, `Namas1`, `Namas2`, `Namas3`, `Namas4`);
+const value10 = (`Lenta`, `Lenta1`, `Lenta2`, `Lenta3`, `Lenta4`);
+const value11 = (`Plyta`, `Plyta1`, `Plyta2`, `Plyta3`, `Plyta4`);
+const opa3 = value3 + ' ' + value4 + ' ' + value5;
+console.log(opa3);
